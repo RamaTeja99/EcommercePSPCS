2b0982bf53394be395e3d93849bb6206951d598a
RamaTeja99 <2200032668@kluniversity.in>
2024-02-02 23:17:15 +0530

    Changed HomePage

--- PSPCSProject/PSPCS/Core/urls.py
+++ PSPCSProject/PSPCS/Core/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from .views import *
+from django.contrib.auth.views import LoginView  # Import the correct LoginView
+
 urlpatterns = [
     path('', homepage, name='homepage'),
+    path('dashboard/', DashBoardView.as_view(), name='dashboard'),
+    path('products/', products, name='products'),
+    path('categories/', categories, name='categories'),
+    path('login/', login, name='login'),
+    path('register/', register, name='register'),
 ]
--- PSPCSProject/PSPCS/Core/views.py
+++ PSPCSProject/PSPCS/Core/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from django.views import View
+from django.contrib.auth.views import LoginView as BaseLoginView
+
+class DashBoardView(View):
+    def get(self, request):
+        return render(request, 'dashboard.html')
+
 def homepage(request):
     return render(request,'homepage.html')
+def products(request):
+    return render(request, 'products.html')
+
+def categories(request):
+    return render(request, 'categories.html')
+
+def login(request):
+    return render(request,'login.html')
+
+def register(request):
+    return render(request,'register.html')
+
