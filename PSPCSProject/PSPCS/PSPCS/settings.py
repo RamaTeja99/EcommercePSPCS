6233ee1bc1fd3eaa138862e80a3afa6acb8b82e7
RamaTeja99 <2200032668@kluniversity.in>
2024-03-07 10:49:44 +0530

    Changed settings

--- PSPCSProject/PSPCS/PSPCS/settings.py
+++ PSPCSProject/PSPCS/PSPCS/settings.py
@@ -76,17 +76,36 @@ WSGI_APPLICATION = 'PSPCS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'ProductSearch',
+#         'USER': 'postgres',
+#         'PASSWORD': 'vamsi308',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'ProductSearch',
         'USER': 'postgres',
-        'PASSWORD': 'vamsi308',
+        'PASSWORD': '25092004',
         'HOST': 'localhost',
         'PORT': '5432',
     }
 }
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'ProductSearch',
+#         'USER': 'postgres',
+#         'PASSWORD': 'charan',
+#         'HOST': 'localhost',
+#         'PORT': '5433',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
