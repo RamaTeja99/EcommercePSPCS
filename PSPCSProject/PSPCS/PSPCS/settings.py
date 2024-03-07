05f82d5e207694abae1e5a7bb1c229b77fb9bec7
VAMSI <vamsi@gmail.com>
2024-03-07 16:44:14 +0530

    register update

--- PSPCSProject/PSPCS/PSPCS/settings.py
+++ PSPCSProject/PSPCS/PSPCS/settings.py
@@ -76,26 +76,26 @@ WSGI_APPLICATION = 'PSPCS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'ProductSearch',
-#         'USER': 'postgres',
-#         'PASSWORD': 'vamsi308',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'ProductSearch',
         'USER': 'postgres',
-        'PASSWORD': '25092004',
+        'PASSWORD': 'vamsi308',
         'HOST': 'localhost',
         'PORT': '5432',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'ProductSearch',
+#         'USER': 'postgres',
+#         'PASSWORD': '25092004',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
