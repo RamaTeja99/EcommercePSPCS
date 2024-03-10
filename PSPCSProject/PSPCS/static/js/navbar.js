938cc49bca16d1c397cb7c240194ba3d3e7a7307
RamaTeja99 <2200032668@kluniversity.in>
2024-03-10 08:23:13 +0530

    Added Cart

--- PSPCSProject/PSPCS/static/js/navbar.js
+++
@@ -1,8 +0,0 @@
-function toggleSearch() {
-    var searchForm = document.querySelector('.navbar .d-flex');
-    if (searchForm.style.display === 'none' || searchForm.style.display === '') {
-        searchForm.style.display = 'flex';
-    } else {
-        searchForm.style.display = 'none';
-    }
-}
