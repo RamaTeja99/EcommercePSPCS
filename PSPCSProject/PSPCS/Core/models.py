928244284df04a10845b1ddf0bdb3f47d8c8b3f3
RamaTeja99 <2200032668@kluniversity.in>
2024-04-26 22:13:41 +0530

    Added Platforms details

--- PSPCSProject/PSPCS/Core/models.py
+++ PSPCSProject/PSPCS/Core/models.py
@@ -21,7 +21,6 @@ class ProductPlatform(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     platform = models.ForeignKey(Platform, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='static/product_images/', null=True, blank=True)
     link = models.URLField(blank=True)
 
     def __str__(self):
